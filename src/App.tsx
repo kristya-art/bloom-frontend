619755058d3dc37bf1fe7ee7c1f35b28070a6a30
Your Name <kristina.salini@dynasoft.ch>
2024-11-25 16:50:52 +0100

    add connection backend-frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+
+//define flower type
+type Flower = {
+  id:number;
+  name:string;
+  color:string;
+  price:number;
+};
 
 
 let name: string = 'pilucho';
 function App() {
-  return (
-  <div className="App ">
-     My name is  {name}!
-    </div>
-  );
+
+const[flowers, setFlowers] = useState<Flower[]>([]);
+
+const language = navigator.language.substring(0,2);
+ 
+
+  // Fetch flowers data
+  fetch('/de/api/flowers')
+  .then(response => {
+      if (!response.ok) {
+          throw new Error(`Error: ${response.status}`);
+      }
+      return response.json();
+  })
+  .then(data => setFlowers(data))
+  .catch(error => console.error('Error fetching flowers:', error));
+
+
+return (
+  <div className="App">
+    <header className="App-header">
+      <div className="App-intro">
+        <h2>JUG List</h2>
+       
+        {flowers.map((flower) => (
+          <div key={flower.id}>{flower.name}</div>
+        ))}
+      </div>
+    </header>
+  </div>
+);
 }
 
 export default App;
